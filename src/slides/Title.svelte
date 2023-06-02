258acbebe29126ffe59cb41334a18fb0d6acf310
Tax3v4der <tax3v4der@gmail.com>
2023-06-02 23:22:39 +0100

    01

--- src/slides/Title.svelte
+++ src/slides/Title.svelte
@@ -1,34 +1,32 @@
 <script>
-    import { onMount } from 'svelte';
- import Slide from '../lib/Slide.svelte';
- import Code from '../lib/Code.svelte';
+  import { onMount } from "svelte";
+  import Slide from "../lib/Slide.svelte";
+  import Code from "../lib/Code.svelte";
 
-    let title;
-    onMount(() => {
-        let timeout;
-        title = new Promise(resolve => {
-            timeout = setTimeout(() => resolve('Whats nice together?'), 1000);
-        });
-        () => clearTimeout(timeout);
+  let title;
+  onMount(() => {
+    let timeout;
+    title = new Promise((resolve) => {
+      timeout = setTimeout(() => resolve("ISLEMMMMMMM"), 1000);
     });
+    () => clearTimeout(timeout);
+  });
 </script>
 
-<Slide>
-    <h2>
-        {#await title}
-            Wait a sec...
-        {:then result}
-            {result}
-        {/await}
-    </h2>
+<Slide data-background="#bb00bb">
+  <h2>
+    {#await title}
+      Wait a sec...
+    {:then result}
+      {result}
+    {/await}
+  </h2>
 
-		<Code lineNumbers trim noescape>
-    {
-    `const name = "hello world";
-    if(name === 'hello') {
-      console.log('world');
+  <Code lineNumbers trim noescape>
+    {`const name = "hello Islem";
+    if(name === 'Islem') {
+      console.log('my world');
     }
-    `
-    }
-		</Code>
+    `}
+  </Code>
 </Slide>
